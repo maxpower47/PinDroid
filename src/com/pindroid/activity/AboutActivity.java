896ea4ab761444752b4af5f94fec89fd9c12dbe1
Matthew Schmidt <matthewrschmidt@gmail.com>
2011-06-09 14:53:56 -0400

    About screen is now a fragment.

--- src/com/pindroid/activity/AboutActivity.java
+++ src/com/pindroid/activity/AboutActivity.java
@@ -21,70 +21,15 @@
 
 package com.pindroid.activity;
 
-import android.app.SearchManager;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.webkit.WebView;
 
 import com.pindroid.R;
 
-public class AboutActivity extends AppBaseActivity {
+public class AboutActivity extends FragmentBaseActivity {
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		
-		setContentView(R.layout.about_view);
-		setTitle(R.string.about_activity_title);
-		
-		WebView content = (WebView) findViewById(R.id.about_text_view);
-		content.loadData(getString(R.string.about_text), "text/html", "utf-8");
-		
-		Intent intent = getIntent();
-		
-		if(Intent.ACTION_SEARCH.equals(intent.getAction())){
-			if(intent.hasExtra(SearchManager.QUERY)){
-				Intent i = new Intent(mContext, MainSearchResults.class);
-				i.putExtras(intent.getExtras());
-				startActivity(i);
-				finish();
-			} else {
-				onSearchRequested();
-			}
-		} else if(Intent.ACTION_VIEW.equals(intent.getAction())) {
-			
-			Uri data = intent.getData();
-			String path = null;
-			String tagname = null;
-			
-			if(data != null) {
-				path = data.getPath();
-				tagname = data.getQueryParameter("tagname");
-			}
-			
-			if(data.getScheme() == null || !data.getScheme().equals("content")){
-				Intent i = new Intent(Intent.ACTION_VIEW, data);
-				
-				startActivity(i);
-				finish();				
-			} else if(path.contains("bookmarks") && TextUtils.isDigitsOnly(data.getLastPathSegment())) {
-				Intent viewBookmark = new Intent(this, ViewBookmark.class);
-				viewBookmark.setData(data);
-				
-				Log.d("View Bookmark Uri", data.toString());
-				startActivity(viewBookmark);
-				finish();
-			} else if(tagname != null) {
-				Intent viewTags = new Intent(this, BrowseBookmarks.class);
-				viewTags.setData(data);
-				
-				Log.d("View Tags Uri", data.toString());
-				startActivity(viewTags);
-				finish();
-			}
-		}
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_view);
 	}
 }
