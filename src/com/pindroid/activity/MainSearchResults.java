948a2b64204a5280b8b8d365dac380614be72925
Matthew Schmidt <matthewrschmidt@gmail.com>
2011-06-17 11:53:06 -0400

    Search results screen is now a fragment.

--- src/com/pindroid/activity/MainSearchResults.java
+++ src/com/pindroid/activity/MainSearchResults.java
@@ -23,108 +23,13 @@ package com.pindroid.activity;
 
 import com.pindroid.R;
 
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
 
-public class MainSearchResults extends AppBaseListActivity {
+public class MainSearchResults extends FragmentBaseActivity {
 
-	private Context mContext;
-	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_search_results);
-		setTitle(R.string.main_search_results_title);
-		
-		String[] MENU_ITEMS = new String[] {getString(R.string.search_results_bookmark),
-				getString(R.string.search_results_tag), getString(R.string.search_results_global_tag)};
-		
-		setListAdapter(new ArrayAdapter<String>(this, R.layout.main_view, MENU_ITEMS));
-		mContext = this;
-
-		final Intent intent = getIntent();
-		
-		if(Intent.ACTION_SEARCH.equals(intent.getAction())){
-			if(intent.hasExtra(SearchManager.QUERY)){
-				Intent i = new Intent(mContext, MainSearchResults.class);
-				i.putExtras(intent.getExtras());
-				startActivity(i);
-				finish();
-			} else {
-				onSearchRequested();
-			}
-		} else if(Intent.ACTION_VIEW.equals(intent.getAction())) {
-			
-			Uri data = intent.getData();
-			String path = null;
-			String tagname = null;
-			
-			if(data != null) {
-				path = data.getPath();
-				tagname = data.getQueryParameter("tagname");
-			}
-			
-			if(data.getScheme() == null || !data.getScheme().equals("content")){
-				Intent i = new Intent(Intent.ACTION_VIEW, data);
-				
-				startActivity(i);
-				finish();				
-			} else if(path.contains("bookmarks") && TextUtils.isDigitsOnly(data.getLastPathSegment())) {
-				Intent viewBookmark = new Intent(this, ViewBookmark.class);
-				viewBookmark.setData(data);
-				
-				Log.d("View Bookmark Uri", data.toString());
-				startActivity(viewBookmark);
-				finish();
-			} else if(tagname != null) {
-				Intent viewTags = new Intent(this, BrowseBookmarks.class);
-				viewTags.setData(data);
-				
-				Log.d("View Tags Uri", data.toString());
-				startActivity(viewTags);
-				finish();
-			}
-		} 
-		
-		ListView lv = getListView();
-		lv.setTextFilterEnabled(true);
-
-		lv.setOnItemClickListener(new OnItemClickListener() {
-		    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		    	if(position == 0){
-		    		
-		    		Intent i = new Intent(mContext, BrowseBookmarks.class);
-		    		i.setAction(Intent.ACTION_SEARCH);
-		    		i.putExtras(intent.getExtras());
-		    		
-		    		startActivity(i);
-		    	} else if(position == 1){
-		    		
-		    		Intent i = new Intent(mContext, BrowseTags.class);
-		    		i.setAction(Intent.ACTION_SEARCH);
-		    		i.putExtras(intent.getExtras());
-		    		
-		    		startActivity(i);
-		    	} else if(position == 2){
-		    		
-		    		Intent i = new Intent(mContext, BrowseBookmarks.class);
-		    		i.setAction(Intent.ACTION_SEARCH);
-		    		i.putExtras(intent.getExtras());
-		    		i.putExtra("username", "global");
-		    		
-		    		startActivity(i);
-		    	}
-		    }
-		});
 	}
 }
