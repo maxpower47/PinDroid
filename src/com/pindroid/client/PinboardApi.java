91a7c6955e5f8a286f192066027de8c7165d4ec0
Matthew Schmidt <matthewrschmidt@gmail.com>
2013-01-04 10:52:04 -0500

    Cleanup.

--- src/com/pindroid/client/PinboardApi.java
+++ src/com/pindroid/client/PinboardApi.java
@@ -21,10 +21,8 @@
 
 package com.pindroid.client;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.TreeMap;
@@ -471,7 +469,6 @@ public class PinboardApi {
 
     	InputStream responseStream = null;
     	final TreeMap<String, String> params = new TreeMap<String, String>();
-    	String response = null;
     	  	
     	responseStream = PinboardApiCall(FETCH_SECRET_URI, params, account, context);
     	SaxTokenParser parser = new SaxTokenParser(responseStream);
@@ -634,38 +631,5 @@ public class PinboardApi {
     	} else {
     		throw new IOException();
     	}
-    }
-    
-    /**
-     * Converts an InputStream to a string.
-     * 
-     * @param is The InputStream to convert.
-     * @return The String retrieved from the InputStream.
-     */
-    private static String convertStreamToString(InputStream is) {
-        /*
-         * To convert the InputStream to String we use the BufferedReader.readLine()
-         * method. We iterate until the BufferedReader return null which means
-         * there's no more data to read. Each line will appended to a StringBuilder
-         * and returned as String.
-         */
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
- 
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sb.toString();
     }
 }
