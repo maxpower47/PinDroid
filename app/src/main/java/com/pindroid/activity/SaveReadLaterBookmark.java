f6c3a899316430aa054acb748f27bc25a8e7d604
Bartłomiej Piech <bartlomiej.piech@mobica.com>
2014-11-01 12:20:43 +0100

    account manager can be local

--- app/src/main/java/com/pindroid/activity/SaveReadLaterBookmark.java
+++ app/src/main/java/com/pindroid/activity/SaveReadLaterBookmark.java
@@ -46,15 +46,13 @@ import android.widget.Toast;
 public class SaveReadLaterBookmark extends Activity {
 	private static final String TAG = "SaveReadLaterBookmark";
 	private PindroidApplication app;
-	private AccountManager accountManager;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 
 		app = (PindroidApplication)getApplicationContext();
-		accountManager = AccountManager.get(this);
-		
+
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
 			requestAccount();
 		} else {
@@ -69,7 +67,7 @@ public class SaveReadLaterBookmark extends Activity {
 			i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 			startActivityForResult(i, Constants.REQUEST_CODE_ACCOUNT_CHANGE);
 		} else if (AccountHelper.getAccountCount(this) > 0) {
-			Account account = accountManager.getAccountsByType(Constants.ACCOUNT_TYPE)[0];
+			Account account = AccountManager.get(this).getAccountsByType(Constants.ACCOUNT_TYPE)[0];
 			app.setUsername(account.name);
 		}
 	}
