06207d3d5d7e46a86a5ecd56697fc8c7c840ddce
Matthew Schmidt <matthewrschmidt@gmail.com>
2015-01-07 15:14:20 -0500

    Convert screen shortcut.

--- app/src/main/java/com/pindroid/activity/ScreenShortcut.java
+++ app/src/main/java/com/pindroid/activity/ScreenShortcut.java
@@ -22,14 +22,10 @@
 package com.pindroid.activity;
 
 import android.accounts.AccountManager;
-import android.content.Context;
 import android.content.Intent;
 import android.content.Intent.ShortcutIconResource;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -37,86 +33,79 @@ import com.pindroid.Constants;
 import com.pindroid.R;
 import com.pindroid.action.IntentHelper;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ItemClick;
+import org.androidannotations.annotations.OnActivityResult;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.small_widget_configure_activity)
 public class ScreenShortcut extends ActionBarActivity {
 
 	private String username = "";
-    private ListView listView;
+    @ViewById(R.id.shortcut_list) ListView listView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.small_widget_configure_activity);
         getSupportActionBar().setTitle(R.string.small_widget_configuration_description);
-
-        listView = (ListView) findViewById(R.id.shortcut_list);
 
 		Intent i = AccountManager.newChooseAccountIntent(null, null, new String[]{Constants.ACCOUNT_TYPE}, false, null, null, null, null);
 		startActivityForResult(i, Constants.REQUEST_CODE_ACCOUNT_CHANGE);
+    }
 
+    @AfterViews
+    void init() {
         String[] MENU_ITEMS = new String[] {getString(R.string.small_widget_my_bookmarks),
                 getString(R.string.small_widget_my_unread),
                 getString(R.string.small_widget_my_notes),
                 getString(R.string.small_widget_add_bookmark),
                 getString(R.string.small_widget_search_bookmarks)};
 
-
         listView.setAdapter(new ArrayAdapter<String>(this, R.layout.widget_configure_view, MENU_ITEMS));
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                final Context context = ScreenShortcut.this;
-
-                Intent shortcutIntent;
-                String shortcutName;
-
-                switch (position){
-                    default:
-                    case 0:
-                        shortcutIntent = IntentHelper.ViewBookmarks(null, username, null, context);
-                        shortcutName = getString(R.string.small_widget_my_bookmarks);
-                        break;
-                    case 1:
-                        shortcutIntent = IntentHelper.ViewUnread(username, context);
-                        shortcutName = getString(R.string.small_widget_my_unread);
-                        break;
-                    case 2:
-                        shortcutIntent = IntentHelper.ViewNotes(username, context);
-                        shortcutName = getString(R.string.small_widget_my_notes);
-                        break;
-                    case 3:
-                        shortcutIntent = IntentHelper.AddBookmark(null, username, context);
-                        shortcutName = getString(R.string.small_widget_add_bookmark);
-                        break;
-                    case 4:
-                        shortcutIntent = IntentHelper.WidgetSearch(username, context);
-                        shortcutName = getString(R.string.small_widget_search_bookmarks);
-                        break;
-                }
-
-
-                final ShortcutIconResource iconResource = ShortcutIconResource.fromContext(context, R.drawable.ic_main);
-                final Intent intent = new Intent();
-                intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-                intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, shortcutName);
-                intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconResource);
-                setResult(RESULT_OK, intent);
-                finish();
-            }
-        });
     }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		return false;
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data){	
-		super.onActivityResult(requestCode, resultCode, data);
-		
-		if(requestCode == Constants.REQUEST_CODE_ACCOUNT_CHANGE){
-			username = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-		}
+    @ItemClick(R.id.shortcut_list)
+    void listItemClicked(int position) {
+        Intent shortcutIntent;
+        String shortcutName;
+
+        switch (position){
+            default:
+            case 0:
+                shortcutIntent = IntentHelper.ViewBookmarks(null, username, null, this);
+                shortcutName = getString(R.string.small_widget_my_bookmarks);
+                break;
+            case 1:
+                shortcutIntent = IntentHelper.ViewUnread(username, this);
+                shortcutName = getString(R.string.small_widget_my_unread);
+                break;
+            case 2:
+                shortcutIntent = IntentHelper.ViewNotes(username, this);
+                shortcutName = getString(R.string.small_widget_my_notes);
+                break;
+            case 3:
+                shortcutIntent = IntentHelper.AddBookmark(null, username, this);
+                shortcutName = getString(R.string.small_widget_add_bookmark);
+                break;
+            case 4:
+                shortcutIntent = IntentHelper.WidgetSearch(username, this);
+                shortcutName = getString(R.string.small_widget_search_bookmarks);
+                break;
+        }
+
+        final ShortcutIconResource iconResource = ShortcutIconResource.fromContext(this, R.drawable.ic_main);
+        final Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, shortcutName);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconResource);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @OnActivityResult(Constants.REQUEST_CODE_ACCOUNT_CHANGE)
+    protected void onChooseAccount(Intent data){
+		username = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
 	}
 }
