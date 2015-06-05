95c1179b729b438f18b43f0b1be66e6204932347
Matthew Schmidt <mschmidt@healthx.com>
2015-06-05 15:23:48 -0400

    Feed recycler view mostly working.

---
+++ app/src/main/java/com/pindroid/listadapter/BookmarkFeedAdapter.java
@@ -0,0 +1,63 @@
+package com.pindroid.listadapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.pindroid.event.BookmarkSelectedEvent;
+import com.pindroid.event.FeedBookmarkSelectedEvent;
+import com.pindroid.model.FeedBookmark;
+import com.pindroid.providers.BookmarkContent;
+import com.pindroid.ui.BookmarkFeedView;
+import com.pindroid.ui.BookmarkFeedView_;
+import com.pindroid.ui.BookmarkView;
+import com.pindroid.ui.BookmarkView_;
+
+import org.androidannotations.annotations.EBean;
+import org.androidannotations.annotations.RootContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.greenrobot.event.EventBus;
+
+@EBean
+public class BookmarkFeedAdapter extends RecyclerAdapter<FeedBookmark, BookmarkFeedView> {
+
+	@RootContext Context context;
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public void setFeedBookmarks(List<FeedBookmark> feedBookmarks) {
+        items = feedBookmarks;
+		notifyDataSetChanged();
+	}
+
+    public FeedBookmark getItemAtPosition(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    protected BookmarkFeedView onCreateItemView(ViewGroup parent, int viewType) {
+        return BookmarkFeedView_.build(context);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewWrapper<BookmarkFeedView> viewHolder, int position) {
+        BookmarkFeedView view = viewHolder.getView();
+        final FeedBookmark bookmark = items.get(position);
+        view.bind(bookmark);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EventBus.getDefault().post(new FeedBookmarkSelectedEvent(bookmark));
+            }
+        });
+    }
+}
